a9083f61a2c068482fa06e5b10d1cd5aa18ab565
Takamasa Fukase <ultrafukase@gmail.com>
2023-09-30 22:03:38 +0900

    カスタムの色定義を追加。色をそれに差し替え

--- app/src/main/java/com/takamasafukase/ar_gunman_android/RankingItem.kt
+++ app/src/main/java/com/takamasafukase/ar_gunman_android/RankingItem.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.sp
@@ -23,31 +24,31 @@ fun RankingItem(rankIndex: Int, ranking: Ranking) {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(horizontal = Dp(16.0f), vertical = Dp(2.0f))
-                .background(Color.Blue)
+                .background(colorResource(id = R.color.customBrown1))
         )
         Spacer(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(horizontal = Dp(30.0f), vertical = Dp(10.0f))
-                .background(Color.Green)
+                .background(colorResource(id = R.color.customBrown2))
         )
         Row(
             verticalAlignment = Alignment.Bottom
         ) {
             Text(
                 rankIndex.toString(),
-                color = Color.White,
+                color = colorResource(id = R.color.paper),
                 fontSize = 22.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier
                     .padding(start = Dp(24.0f), bottom = Dp(6.0f))
                     .size(width = Dp(32.0f), height = Dp(22.0f))
-                    .background(Color.Red)
+                    .background(colorResource(id = R.color.goldLeaf))
                     .wrapContentSize(align = Alignment.Center, unbounded = true)
             )
             Text(
                 text = ranking.score.toString(),
-                color = Color.White,
+                color = colorResource(id = R.color.paper),
                 fontSize = 22.sp,
                 fontWeight = FontWeight.Normal,
                 modifier = Modifier
@@ -55,7 +56,7 @@ fun RankingItem(rankIndex: Int, ranking: Ranking) {
             )
             Text(
                 text = ranking.user_name,
-                color = Color.White,
+                color = colorResource(id = R.color.paper),
                 fontSize = 38.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier
