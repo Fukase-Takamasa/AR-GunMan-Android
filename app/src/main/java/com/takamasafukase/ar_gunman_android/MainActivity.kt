633eb71948bbecf1ae7d79274e326ef03d520254
Takamasa Fukase <ultrafukase@gmail.com>
2023-09-30 23:05:15 +0900

    ランキングの縦横幅を画面サイズから割合で縮小してセンター配置する実装を追加

--- app/src/main/java/com/takamasafukase/ar_gunman_android/MainActivity.kt
+++ app/src/main/java/com/takamasafukase/ar_gunman_android/MainActivity.kt
@@ -3,20 +3,16 @@ package com.takamasafukase.ar_gunman_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
 import com.takamasafukase.ar_gunman_android.ui.theme.ARGunManAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,7 +39,23 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
     val state by viewModel.state.collectAsState()
-    RankingListView(list = state.rankings)
+    val screenWidth = LocalConfiguration.current.screenWidthDp
+    val screenHeight = LocalConfiguration.current.screenHeightDp
+    Box(
+        contentAlignment = Alignment.Center,
+    ) {
+        Box(
+            modifier = Modifier
+                .size(
+                    width = Dp((screenWidth * 0.6).toFloat()),
+                    height = Dp((screenHeight * 0.8).toFloat())
+                )
+        ) {
+            RankingListView(
+                list = state.rankings
+            )
+        }
+    }
 }
 
 @Composable
