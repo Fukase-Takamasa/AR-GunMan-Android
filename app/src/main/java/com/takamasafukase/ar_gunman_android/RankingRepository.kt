3a08916e1e93a77c79d6c8f513529f117ce2c1fe
Takamasa Fukase <ultrafukase@gmail.com>
2023-10-01 13:06:40 +0900

    ランキング取得のAPIロジックをrepositoryに移動

---
+++ app/src/main/java/com/takamasafukase/ar_gunman_android/RankingRepository.kt
@@ -0,0 +1,39 @@
+package com.takamasafukase.ar_gunman_android
+
+import android.os.Handler
+import android.os.Looper
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class RankingRepository {
+    private val db = Firebase.firestore
+
+    fun getRankings(
+        onData: (List<Ranking>) -> Unit,
+        onError: (Exception) -> Unit
+    ) {
+        db.collection("worldRanking")
+            .orderBy("score", Query.Direction.DESCENDING)
+            .get()
+            .addOnSuccessListener { documents ->
+                onData(
+                    documents.toObjects(Ranking::class.java)
+                )
+            }
+            .addOnFailureListener { error ->
+                onError(error)
+            }
+    }
+
+    fun getDummyRankings(
+        onData: (List<Ranking>) -> Unit,
+        onError: (Exception) -> Unit
+    ) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            onData(
+                (1..150).map { Ranking(score = 98.765, user_name = "ユーザー名") }
+            )
+        }, 1000)
+    }
+}
