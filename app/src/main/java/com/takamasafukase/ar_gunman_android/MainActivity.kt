79d1979c188925af7a0351655617eb4e3cfdfc43
Takamasa Fukase <ultrafukase@gmail.com>
2023-10-01 13:07:36 +0900

    ランキング取得ロジックをrepository経由に差し替え、呼び出しもVMのinit内に移動

--- app/src/main/java/com/takamasafukase/ar_gunman_android/MainActivity.kt
+++ app/src/main/java/com/takamasafukase/ar_gunman_android/MainActivity.kt
@@ -1,29 +1,26 @@
 package com.takamasafukase.ar_gunman_android
 
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.Dp
 import com.takamasafukase.ar_gunman_android.ui.theme.ARGunManAndroidTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel = MainViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel = MainViewModel()
-        viewModel.getDummyRankings()
 
         setContent {
             ARGunManAndroidTheme {
--- app/src/main/java/com/takamasafukase/ar_gunman_android/MainViewModel.kt
+++ app/src/main/java/com/takamasafukase/ar_gunman_android/MainViewModel.kt
@@ -1,48 +1,23 @@
 package com.takamasafukase.ar_gunman_android
 
-import android.os.Looper
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.firestore.Query
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import android.os.Handler
 
 class MainViewModel {
     private val _state = MutableStateFlow(MainViewState(listOf()))
     val state = _state.asStateFlow()
+    private val rankingRepository = RankingRepository()
 
-    private val db = Firebase.firestore
-
-
-    fun getRankings() {
-        db.collection("worldRanking")
-            .orderBy("score", Query.Direction.DESCENDING)
-            .get()
-            .addOnSuccessListener { documents ->
-                val rankings = documents.toObjects(Ranking::class.java)
-                _state.value = _state.value.copy(rankings = rankings)
-            }
-            .addOnFailureListener { error ->
-                println("error: $error")
-            }
+    init {
+        getRankings()
     }
 
-    fun getDummyRankings() {
-        val list = listOf(
-            Ranking(score = 98.765, user_name = "なまえ"),
-            Ranking(score = 98.765, user_name = "なまえ"),
-            Ranking(score = 98.765, user_name = "なまえ"),
-            Ranking(score = 98.765, user_name = "なまえ"),
-            Ranking(score = 98.765, user_name = "なまえ"),
-            Ranking(score = 98.765, user_name = "なまえ"),
-            Ranking(score = 98.765, user_name = "なまえ"),
-            Ranking(score = 98.765, user_name = "なまえ"),
-            Ranking(score = 98.765, user_name = "なまえ"),
-            Ranking(score = 98.765, user_name = "なまえ"),
+    private fun getRankings() {
+        rankingRepository.getDummyRankings(
+            onData = {
+                _state.value = _state.value.copy(rankings = it)
+            },
+            onError = {}
         )
-        Handler(Looper.getMainLooper()).postDelayed({
-            _state.value = _state.value.copy(rankings = list)
-        }, 1000)
     }
 }
