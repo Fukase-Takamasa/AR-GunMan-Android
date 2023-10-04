851764683c683c9e90e5529fd99143496c143dd1
Takamasa Fukase <ultrafukase@gmail.com>
2023-10-04 22:03:16 +0900

    ランキング画面の残りのUIを追加

--- app/src/main/java/com/takamasafukase/ar_gunman_android/MainActivity.kt
+++ app/src/main/java/com/takamasafukase/ar_gunman_android/MainActivity.kt
@@ -3,17 +3,24 @@ package com.takamasafukase.ar_gunman_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.takamasafukase.ar_gunman_android.ui.theme.ARGunManAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,29 +57,99 @@ fun MainScreen(viewModel: MainViewModel) {
                     height = Dp((screenHeight * 0.8).toFloat())
                 )
         ) {
-            RankingListView(list = state.rankings)
+            BackgroundBorderView()
+            Column(
+                modifier = Modifier
+                    .padding(start = 10.dp, end = 10.dp, bottom = 10.dp)
+            ) {
+                TitleView()
+                RankingListView(list = state.rankings)
+            }
+            if (state.rankings.isEmpty()) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    CircularProgressIndicator(color = colorResource(id = R.color.paper))
+                }
+            }
         }
     }
 }
 
+// リストビューの上のビュー
+@Composable
+fun TitleView() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 8.dp)
+            .height(30.dp)
+            .background(
+                color = colorResource(id = R.color.goldLeaf),
+                shape = RoundedCornerShape(5.dp)
+            )
+    ) {
+        Text(
+            "WORLD RANKING",
+            color = colorResource(id = R.color.blackSteel),
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(align = Alignment.Center, unbounded = true)
+        )
+        IconButton(
+            onClick = {
+                // TODO
+            },
+            modifier = Modifier
+                .width(60.dp)
+                .align(Alignment.CenterEnd)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Close,
+                contentDescription = ""
+            )
+        }
+    }
+}
+
+// リストビューの背後に重なっている枠線だけのビュー
+@Composable
+fun BackgroundBorderView() {
+    Spacer(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 15.dp)
+            .border(
+                width = 5.dp,
+                color = colorResource(id = R.color.customBrown1),
+                shape = RoundedCornerShape(2.dp)
+            )
+    )
+}
+
+// リストビューの部分
 @Composable
 fun RankingListView(list: List<Ranking>) {
-    if (list.isEmpty()) {
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier
-                .fillMaxSize(),
-        ) {
-            CircularProgressIndicator(color = colorResource(id = R.color.paper))
-        }
-    } else {
-        LazyColumn {
-            itemsIndexed(list) { index, ranking ->
-                RankingItem(
-                    rankIndex = index + 1,
-                    ranking = ranking
-                )
+    LazyColumn(
+        modifier = Modifier
+            .border(
+                width = 7.dp,
+                color = colorResource(id = R.color.goldLeaf),
+                shape = RoundedCornerShape(size = 3.dp)
+            )
+    ) {
+        itemsIndexed(list) { index, ranking ->
+            if (index == 0) {
+                Spacer(modifier = Modifier.height(10.dp))
             }
+            RankingItem(
+                rankIndex = index + 1,
+                ranking = ranking
+            )
         }
     }
 }
--- app/src/main/java/com/takamasafukase/ar_gunman_android/RankingItem.kt
+++ app/src/main/java/com/takamasafukase/ar_gunman_android/RankingItem.kt
@@ -1,7 +1,9 @@
 package com.takamasafukase.ar_gunman_android
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -9,60 +11,116 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun RankingItem(rankIndex: Int, ranking: Ranking) {
     Box(
+        // ベースとなる領域を確保
         modifier = Modifier
             .fillMaxSize()
-            .height(height = Dp(60.0f))
+            .height(height = 60.dp)
             .background(Color.Transparent)
     ) {
+        // 背景1
         Spacer(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = Dp(16.0f), vertical = Dp(2.0f))
-                .background(colorResource(id = R.color.customBrown1))
+                .padding(horizontal = 16.dp, vertical = 2.dp)
+                .background(
+                    color = colorResource(id = R.color.customBrown1),
+                )
+                .border(
+                    width = 4.dp,
+                    color = colorResource(id = R.color.goldLeaf),
+                    shape = RoundedCornerShape(3.dp)
+                )
         )
+        // 四隅の白い点
+        whiteCornerSquares()
+        // 背景2
         Spacer(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(horizontal = Dp(30.0f), vertical = Dp(10.0f))
+                .padding(horizontal = 30.dp, vertical = 10.dp)
                 .background(colorResource(id = R.color.customBrown2))
         )
         Row(
             verticalAlignment = Alignment.Bottom
         ) {
+            // 順位
             Text(
                 rankIndex.toString(),
                 color = colorResource(id = R.color.paper),
                 fontSize = 22.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier
-                    .padding(start = Dp(24.0f), bottom = Dp(6.0f))
-                    .size(width = Dp(32.0f), height = Dp(22.0f))
+                    .padding(start = 24.dp, bottom = 6.dp)
+                    .size(width = 32.dp, height = 22.dp)
                     .background(colorResource(id = R.color.goldLeaf))
                     .wrapContentSize(align = Alignment.Center, unbounded = true)
             )
+            // スコア
             Text(
                 text = ranking.score.toString(),
                 color = colorResource(id = R.color.paper),
                 fontSize = 22.sp,
                 fontWeight = FontWeight.Normal,
                 modifier = Modifier
-                    .padding(start = Dp(8.0f), bottom = Dp(10.0f))
+                    .padding(start = 8.dp, bottom = 10.dp)
             )
+            // ユーザー名
             Text(
                 text = ranking.user_name,
                 color = colorResource(id = R.color.paper),
                 fontSize = 38.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier
-                    .padding(start = Dp(10.0f), bottom = Dp(10.0f))
+                    .padding(start = 10.dp, bottom = 10.dp)
             )
         }
     }
 }
 
+@Composable
+fun whiteCornerSquares() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Spacer(
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .size(width = 4.dp, height = 2.dp)
+                .offset(x = 18.dp, y = 10.dp)
+                .background(
+                    color = colorResource(id = R.color.paper),
+                )
+        )
+        Spacer(
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .size(width = 4.dp, height = 2.dp)
+                .offset(x = (-18).dp, y = 10.dp)
+                .background(
+                    color = colorResource(id = R.color.paper),
+                )
+        )
+        Spacer(
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .size(width = 4.dp, height = 2.dp)
+                .offset(x = 18.dp, y = (-10).dp)
+                .background(
+                    color = colorResource(id = R.color.paper),
+                )
+        )
+        Spacer(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .size(width = 4.dp, height = 2.dp)
+                .offset(x = (-18).dp, y = (-10).dp)
+                .background(
+                    color = colorResource(id = R.color.paper),
+                )
+        )
+    }
+}
