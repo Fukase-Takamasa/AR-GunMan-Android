c3c64ff7620be736944f8a57e13ffbbd83703cf7
Takamasa Fukase <ultrafukase@gmail.com>
2023-12-10 13:12:45 +0900

    Unityとの相互通信箇所をJSONパースを噛ませて、Android側では文字列直ではなくdata classとして扱える様に差し替え

--- app/src/main/java/com/takamasafukase/ar_gunman_android/viewModel/GameViewModel.kt
+++ app/src/main/java/com/takamasafukase/ar_gunman_android/viewModel/GameViewModel.kt
@@ -5,6 +5,11 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.takamasafukase.ar_gunman_android.Model.AndroidToUnityMessage
+import com.takamasafukase.ar_gunman_android.Model.AndroidToUnityMessageEventType
+import com.takamasafukase.ar_gunman_android.Model.UnityToAndroidMessage
+import com.takamasafukase.ar_gunman_android.Model.UnityToAndroidMessageEventType
+import com.takamasafukase.ar_gunman_android.Model.WeaponType
 import com.takamasafukase.ar_gunman_android.manager.AudioManager
 import com.takamasafukase.ar_gunman_android.manager.MotionDetector
 import com.takamasafukase.ar_gunman_android.R
@@ -12,6 +17,9 @@ import com.takamasafukase.ar_gunman_android.UnityToAndroidMessenger
 import com.unity3d.player.UnityPlayer
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import java.lang.ref.WeakReference
 
 data class GameViewState(
@@ -39,6 +47,14 @@ class GameViewModel(
 
     override fun onMessageReceivedFromUnity(message: String) {
         Log.d("Android", "ログAndroid: GameVM onMessageReceivedFromUnity message: $message")
+
+        val fromUnityMessage = Json.decodeFromString<UnityToAndroidMessage>(message)
+        when (fromUnityMessage.eventType) {
+            UnityToAndroidMessageEventType.TARGET_HIT -> {
+                // TODO: スコアの加算処理
+                audioManager.playSound(R.raw.head_shot)
+            }
+        }
     }
 
     // Unityビューを起動後にUnityロゴのスプラッシュが出るので、その間は黒背景とインジケータで隠す
@@ -54,8 +70,16 @@ class GameViewModel(
             onDetectPistolFiringMotion = {
                 Log.d("Android", "ログAndroid: onDetectPistolFiringMotion")
                 audioManager.playSound(R.raw.pistol_fire)
+
+                // ピストル射撃命令のメッセージを作成
+                val toUnityMessage = AndroidToUnityMessage(
+                    eventType = AndroidToUnityMessageEventType.FIRE_WEAPON,
+                    weaponType = WeaponType.PISTOL,
+                )
+                // JSON文字列に変換
+                val jsonString = Json.encodeToString(toUnityMessage)
                 // Unityへ通知を送る
-                UnityPlayer.UnitySendMessage("XR Origin", "ShootBullet", "")
+                UnityPlayer.UnitySendMessage("XR Origin", "OnReceiveMessageFromAndroid", jsonString)
             },
             onDetectPistolReloadingMotion = {
                 Log.d("Android", "ログAndroid: onDetectPistolReloadingMotion")
